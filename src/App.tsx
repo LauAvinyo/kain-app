81891cf7a911e787919b58d1f86333fef51c07cb
LauAvinyo <lauavino@embl.es>
2022-10-26 18:43:27 +0200

    /landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import 'dracula-ui/styles/dracula-ui.css'
+import { Heading, Card, Text } from 'dracula-ui'
+
+const Gretting = () => {
+  return (
+    <Card color="cyanGreen" p="sm" m="lg">
+      <Heading color="black">Hi there!</Heading>
+      <Text color="black">
+        Welcome to Kain. At somemoment this will be a very cool image analysis
+        platform. But for now is just some text.
+      </Text>
+    </Card>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Gretting />
     </div>
   )
 }
